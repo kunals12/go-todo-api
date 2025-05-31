00a300f078aaef5a34002eccac859c5dabe66881
kunals12 <work.kunal12@gmail.com>
2025-05-31 17:57:39 +0530

    fix: not found route

--- cmd/main.go
+++ cmd/main.go
@@ -18,5 +18,9 @@ func main() {
 	app.Use(recover.New())
 	routes.SetupTodoRoutes(app)
 	routes.UserRoutes(app)
+	// 404 Handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
 	log.Fatal(app.Listen(":3000"))
 }
